e2ac8969e22bf3ef78e2b1f022f5437dde245d8e
Arham Suhail <arham.suhail@gmail.com>
2017-03-18 12:43:18 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+// console.log("string of text")
+// document.write("I'm going to write something")
+// alert("Hello Pop Up Annoying")
+
+// var username = prompt("What's is your username")
+// console.log(username)
+
+// var num = 1
+
+// var boolean = true
+
+// var string = "Hello "
+
+// var array = [num, string, boolean, 1, "string", false]
+
+// console.log (string + "there! This is my favorite!")
+
+// console.log (num + boolean + string + false + array + 100)
+
+// var name = prompt("Whats your name")
+// var num1 = 10
+// var num2 = 3
+// var num3 = "6"
+// var numAnswer = 12
+// var numGuessed = Number( prompt('Choose a number between 1 - 100'))
+
+
+// if( numGuessed == numAnswer ) {
+// 	//IF conditional is true run this code
+// 	console.log("You guessed correctly!")
+// } else if (numGuessed > numAnswer){
+// 	console.log("Answer is too high")
+
+// } else if (numGuessed < numAnswer){
+// 	console.log("Answer is too low!")
+// }
+// for (var i = 0; i < 20; i++){
+// 	console.log(i)
+// }
+
+// var username = prompt("What's your name again?")
+
+// while (username != 'matt'){
+// 	username = prompt("Whats your name again?")
+// }
+
+// function myFunction(){
+
+// }
+
+// var num1 = 12
+// var num2 = 10
+
+// function addNum(variable1, variable2){
+// 	document.write(variable1 + variable2)
+// }
+
+// addNum(100,200)
+// create variable
+// var num1 = multiply(4,3)
+// // define the function
+// function multiply(a,b){
+// 	// stop the function and do this
+// 	return (a*b)
+// }
+// document.write(num1)
+for(i =0; i <=100; i+=5){
+	console.log(i)
+}
+
